978594417e611c3ea1082b2bb7d9700e04c804d0
MiguelHigueraDev <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-03-13 23:20:37 -0300

    feat: finish basic layout

--- src/components/DiscordCard.tsx
+++ src/components/DiscordCard.tsx
@@ -1,12 +1,31 @@
 import './DiscordCard.css'
 const DiscordCard = () => {
   return (
-    <div className="discord-card-body">
-        <h1 className="discord-card-username text">@misfitdude</h1>
-        <div className="discord-status-container">
-            <p className="discord-card-status text">Best HTML programmer</p>
+    <div className="discord-card-border">
+        <div className="discord-banner-container">
+            <img src="banner.png" className="discord-banner-image" />
+        </div>
+        <div className="discord-card-outer-body">
+            <div className="discord-card-inner-body">
+                <div>
+                    <h1 className="discord-card-displayname text">Misfit</h1>
+                    <h2 className="discord-card-username text">misfitdude</h2>
+                    <div className="discord-status-container">
+                        <img src="raccoon.svg" className="discord-status-image" />
+                        <p className="discord-status-text text">Best HTML programmer</p>
+                    </div>
+                </div>
+                <div className="discord-card-separator"></div>
+                <div>
+
+                </div>
+
+                
+            </div>
         </div>
     </div>
+
+
   )
 }
 
