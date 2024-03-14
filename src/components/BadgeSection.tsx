7073bb220c71e6e6be9b67a637cf92c749a1dfe2
MiguelHigueraDev <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-03-14 20:56:20 -0300

    fix: add key to BadgeSection

--- src/components/BadgeSection.tsx
+++ src/components/BadgeSection.tsx
@@ -4,7 +4,7 @@ const BadgeSection = ({ badges }: { badges: Badge[] }) => {
   return (
     <ul className="discord-badges-container">
       {badges.map((badge) => (
-        <li>
+        <li key={badge.name}>
           <img src={badge.iconUrl} className="discord-badge" alt={badge.name} />
         </li>
       ))}
