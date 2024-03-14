571f05f7dafaafc4ce6802708a16ffd1ddc90f7f
MiguelHigueraDev <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-03-14 20:21:47 -0300

    feat: add useAutosizeTextArea

---
+++ src/hooks/useAutosizeTextArea.ts
@@ -0,0 +1,15 @@
+import { useEffect } from "react"
+
+// Credit: https://medium.com/@oherterich/creating-a-textarea-with-dynamic-height-using-react-and-typescript-5ed2d78d9848
+
+const useAutosizeTextArea = (textAreaRef: HTMLTextAreaElement | null, value: string) => {
+    useEffect(() => {
+        if (textAreaRef) {
+            textAreaRef.style.height = "0px";
+            const scrollHeight = textAreaRef.scrollHeight;
+            textAreaRef.style.height = scrollHeight + "px";
+        }
+    }, [textAreaRef, value]);
+}
+
+export default useAutosizeTextArea
