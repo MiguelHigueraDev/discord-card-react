05d2785ade3910680c0592fb5e6114d9699715f8
MiguelHigueraDev <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-03-13 21:31:48 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import DiscordCard from "./components/DiscordCard"
+import "./App.css"
+
+function App() {
+  return (
+    <div className="main-container">
+      <DiscordCard />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/DiscordCard.tsx
@@ -0,0 +1,13 @@
+import './DiscordCard.css'
+const DiscordCard = () => {
+  return (
+    <div className="discord-card-body">
+        <h1 className="discord-card-username text">@misfitdude</h1>
+        <div className="discord-status-container">
+            <p className="discord-card-status text">Best HTML programmer</p>
+        </div>
+    </div>
+  )
+}
+
+export default DiscordCard
