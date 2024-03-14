a2df0c99f96ae730115ea2599caa2b44cf31cb8b
MiguelHigueraDev <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-03-14 17:24:07 -0300

    chore: add return type to DiscordLink

--- src/components/DiscordLink.tsx
+++ src/components/DiscordLink.tsx
@@ -2,10 +2,10 @@
  * Renders a Discord link.
  *
  * @param {string} href - The URL of the link
- * @param {string} [text] - The text to display for the link, defaults to the link URL if not provided
+ * @param {string} text - The text to display for the link, defaults to the link URL if not provided
  * @return {JSX.Element} The rendered Discord link component
  */
-const DiscordLink = ({ href, text }: { href: string; text?: string }) => {
+const DiscordLink = ({ href, text }: { href: string; text?: string }): JSX.Element => {
   return (
     <>
       {text ? (
