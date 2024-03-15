fd17464c87018b4ad09ff0997ac7a463b113d622
MiguelHigueraDev <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-03-14 21:30:08 -0300

    feat: add children to DiscordCard

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,66 @@
-import DiscordCard from "./components/DiscordCard"
-import "./App.css"
+import DiscordCard from "./components/DiscordCard";
+import "./App.css";
+import DiscordLink from "./components/DiscordLink";
+import MemberSinceSection from "./components/MemberSinceSection";
+import NoteSection from "./components/NoteSection";
+import Role from "./components/Role";
+import RoleSection from "./components/RoleSection";
+import Separator from "./components/Separator";
+import StatusSection from "./components/StatusSection";
+import MessageSection from "./components/MessageSection";
+import AboutMeSection from "./components/AboutMeSection";
+import BasicInfoSection from "./components/BasicInfoSection";
+import { useState } from "react";
 
 function App() {
+  const [note, setNote] = useState("");
+  const [message, setMessage] = useState("");
+
+  function handleNoteChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
+    setNote(event.target.value);
+  }
+
+  function handleMessageChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
+    setMessage(event.target.value);
+  }
   return (
     <div className="main-container">
-      <DiscordCard imageUrl="pfp.webp" bannerUrl="banner.png" primaryColor="#007777" accentColor="#8500d3" />
+      <DiscordCard
+        imageUrl="pfp.webp"
+        bannerUrl="banner.png"
+        primaryColor="#007777"
+        accentColor="#8500d3"
+      >
+        <BasicInfoSection
+          displayname="Misfit"
+          username="misfitdude"
+          pronouns="he/him"
+        />
+        <StatusSection iconUrl="raccoon.svg" status="Best HTML programmer" />
+        <Separator />
+        <AboutMeSection>
+          <p>(25 y/o) Just a nerd who loves coding and technology</p>
+          <p style={{ marginBottom: 10 }}>INTP</p>
+          <DiscordLink href="https://www.youtube.com/watch?v=NN1OtIJu_Bk" />
+          <p>🦝❤️🦉</p>
+        </AboutMeSection>
+        <MemberSinceSection discordJoinDate="20 Jul 2016" />
+        <RoleSection>
+          <Role role="JavaScript" color="#f7df1e" />
+          <Role role="TypeScript" color="#007acc" />
+          <Role role="PHP" color="#4f3e66" />
+          <Role role="CSS" color="#4372c4" />
+          <Role role="HTML" color="#ed731c" />
+        </RoleSection>
+        <NoteSection note={note} handleInput={handleNoteChange} />
+        <MessageSection
+          message={message}
+          handleInput={handleMessageChange}
+          placeholder="Message @Misfit"
+        />
+      </DiscordCard>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/DiscordCard.tsx
+++ src/components/DiscordCard.tsx
@@ -1,15 +1,5 @@
-import { useState } from "react";
-import AboutMeSection from "./AboutMeSection";
-import BasicInfoSection from "./BasicInfoSection";
 import "./DiscordCard.css";
-import DiscordLink from "./DiscordLink";
-import MemberSinceSection from "./MemberSinceSection";
-import NoteSection from "./NoteSection";
-import Role from "./Role";
-import RoleSection from "./RoleSection";
-import Separator from "./Separator";
-import StatusSection from "./StatusSection";
-import MessageSection from "./MessageSection";
+
 import BadgeSection from "./BadgeSection";
 
 const DiscordCard = ({
@@ -17,22 +7,14 @@ const DiscordCard = ({
   bannerUrl,
   primaryColor,
   accentColor,
+  children
 }: {
   imageUrl: string;
   bannerUrl: string;
   primaryColor: string;
   accentColor: string;
+  children: React.JSX.Element | React.JSX.Element[]
 }) => {
-  const [note, setNote] = useState("");
-  const [message, setMessage] = useState("");
-
-  function handleNoteChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
-    setNote(event.target.value);
-  }
-
-  function handleMessageChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
-    setMessage(event.target.value);
-  }
 
   return (
     <div
@@ -60,33 +42,7 @@ const DiscordCard = ({
       </div>
       <div className="discord-card-outer-body">
         <div className="discord-card-inner-body">
-          <BasicInfoSection
-            displayname="Misfit"
-            username="misfitdude"
-            pronouns="he/him"
-          />
-          <StatusSection iconUrl="raccoon.svg" status="Best HTML programmer" />
-          <Separator />
-          <AboutMeSection>
-            <p>(25 y/o) Just a nerd who loves coding and technology</p>
-            <p style={{ marginBottom: 10 }}>INTP</p>
-            <DiscordLink href="https://www.youtube.com/watch?v=NN1OtIJu_Bk" />
-            <p>🦝❤️🦉</p>
-          </AboutMeSection>
-          <MemberSinceSection discordJoinDate="20 Jul 2016" />
-          <RoleSection>
-            <Role role="JavaScript" color="#f7df1e" />
-            <Role role="TypeScript" color="#007acc" />
-            <Role role="PHP" color="#4f3e66" />
-            <Role role="CSS" color="#4372c4" />
-            <Role role="HTML" color="#ed731c" />
-          </RoleSection>
-          <NoteSection note={note} handleInput={handleNoteChange} />
-          <MessageSection
-            message={message}
-            handleInput={handleMessageChange}
-            placeholder="Message @Misfit"
-          />
+            {children}
         </div>
       </div>
     </div>
