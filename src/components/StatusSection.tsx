f3f97a39389dab513b4ba6ccb4a3f1117ddd09e8
MiguelHigueraDev <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-03-15 15:59:09 -0300

    fix: add empty alt to StatusSection

--- src/components/StatusSection.tsx
+++ src/components/StatusSection.tsx
@@ -9,7 +9,7 @@ const StatusSection = ({
 }) => {
   return (
     <section className="discord-status-container">
-      {iconUrl && <img src={iconUrl} className="discord-status-image" />}
+      {iconUrl && <img src={iconUrl} className="discord-status-image" alt="" />}
       {emoji && <p className="discord-status-image">{emoji}</p>}
       <p className="discord-status-text text">{status}</p>
     </section>
