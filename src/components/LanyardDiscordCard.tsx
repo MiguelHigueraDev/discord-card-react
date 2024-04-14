63ecee6187c27b1611ab6a300c1bae63fbf68e7d
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-04-14 18:20:00 -0400

    feat(LanyardDiscordCard): add GameSection props to support elapsed time display

--- src/components/LanyardDiscordCard.tsx
+++ src/components/LanyardDiscordCard.tsx
@@ -40,6 +40,9 @@ const LanyardDiscordCard = ({
   showSpotify = true,
   showGames = true,
   priority = "default",
+  displayElapsedTime = true,
+  timeElapsedText = "elapsed",
+  timeAlignment = "left",
   children,
 }: {
   userId: string;
@@ -61,6 +64,9 @@ const LanyardDiscordCard = ({
   showSpotify?: boolean;
   showGames?: boolean;
   priority?: ActivityPriority;
+  displayElapsedTime?: boolean;
+  timeElapsedText?: string;
+  timeAlignment?: "left" | "right";
   children?: React.JSX.Element | React.JSX.Element[];
 }) => {
   const { status: lanyard } = useLanyard({
@@ -211,8 +217,10 @@ const LanyardDiscordCard = ({
                     : null,
                 },
               })}
-              // Only render elapsed time if not null
-              {...(currentGame.timestamps?.start && {
+              // Only render elapsed time if not null and displayElapsedTime is true
+              elapsedText={timeElapsedText}
+              timeAlignment={timeAlignment}
+              {...(currentGame.timestamps?.start && displayElapsedTime && {
                 startTime: currentGame.timestamps.start,
               })}
             />
