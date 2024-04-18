7a095755566c4aa4e3a04d10d3b91303efa8335d
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-04-18 18:22:31 -0400

    refactor(RoleSection): shorten code and remove p element to improve semantics

--- src/components/RoleSection.tsx
+++ src/components/RoleSection.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { Role } from "../interfaces/Role";
+import { Role as RoleItem } from "../interfaces/Role";
 import BaseSection from "./BaseSection";
 import SectionTitle from "./SectionTitle";
+import Role from "./Role";
 
 /**
  * Render a section for displaying roles with an optional alternative title and list of roles.
@@ -18,7 +19,7 @@ const RoleSection = ({
 }: {
   title?: string;
   children?: React.JSX.Element | React.JSX.Element[];
-  roles?: Role[];
+  roles?: RoleItem[];
 }) => {
   const childrenCount = React.Children.count(children);
 
@@ -28,16 +29,7 @@ const RoleSection = ({
       <ul className="flex flex-wrap gap-[6px] mb-3">
         {roles &&
           roles.map((role, index) => (
-            <li
-              key={index}
-              className="bg-[#4b4b4b8c] py-[1px] px-[8px] rounded-[4px] flex items-center gap-[6px] select-none border-[0.5px] border-[#8887876a]"
-            >
-              <div
-                className="w-[10px] h-[10px] rounded-full"
-                style={{ backgroundColor: role.color }}
-              ></div>
-              <p className="text-[0.8rem]">{role.name}</p>
-            </li>
+            <Role key={index} role={role.name} color={role.color} />
           ))}
         {children}
       </ul>
