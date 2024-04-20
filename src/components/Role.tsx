77ce9c07c0d6b2e6f4bdf75f0911babcf6992f0b
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-04-20 00:11:10 -0400

    fix(Role): increased circle size and decreased gap

--- src/components/Role.tsx
+++ src/components/Role.tsx
@@ -9,10 +9,10 @@ const Role = ({ role, color }: { role: string; color: string }) => {
   return (
     <li className="bg-[#4b4b4b40] py-[0px] px-[8px] rounded-[4px] flex items-center gap-[6px] select-none border-[0.5px] border-[#8887876a]">
       <div
-        className="w-[10px] h-[10px] rounded-full"
+        className="w-[12px] h-[12px] rounded-full"
         style={{ backgroundColor: color }}
       ></div>
-      <div className="text-[0.75rem]">{role}</div>
+      <div className="text-[0.75rem] p-0 m-0">{role}</div>
     </li>
   );
 };
