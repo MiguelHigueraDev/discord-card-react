d7bb480baf6c3f013ca6f08bc26be1fe2df45326
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-05-16 15:06:16 -0400

    fix: role improvements 2: electric boogaloo

--- src/components/Role.tsx
+++ src/components/Role.tsx
@@ -7,12 +7,12 @@
  */
 const Role = ({ role, color }: { role: string; color: string }) => {
   return (
-    <li className="bg-[#4b4b4b40] py-[0px] px-[8px] rounded-[4px] flex items-center gap-[6px] select-none border-[0.5px] border-[#8887876a]">
+    <li className="bg-[#4b4b4b40] p-2 rounded-[4px] h-[25px] flex items-center justify-center gap-[5px] select-none border-[0.5px] border-[#8887876a]">
       <div
         className="w-[12px] h-[12px] rounded-full"
         style={{ backgroundColor: color }}
       ></div>
-      <div className="text-[0.72rem] p-0 m-0">{role}</div>
+      <div className="text-[0.73rem] p-0 m-0 text-center">{role}</div>
     </li>
   );
 };
